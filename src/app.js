0ee59cdade8f2c4501b05deb8d5caf53cfc17a2b
COFLY47 <rpetrovic420@gmail.com>
2024-10-22 18:26:20 +0200

    add infinite scroll

--- src/app.js
+++ src/app.js
@@ -1,19 +1,27 @@
-document.getElementById('search-btn').addEventListener('click', () => {
+  let currentPage = 1;
+  let totalPages = 0;
+  let isLoading = false;
+
+  document.getElementById('search-btn').addEventListener('click', () => {
     const query = document.getElementById('search-input').value;
+    console.log(query)
     searchMovies(query);
   });
 
   async function searchMovies(query) {
-    console.log(query)
+    if (isLoading) return;  
+       isLoading = true;
     const apiKey = 'c86dfea7';
-    const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=c86dfea7`);
+    const response = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=${apiKey}&page=${currentPage}`);
     const data = await response.json();
+    totalPages = Math.ceil(data.totalResults / 10);
     displayMovies(data.Search);
+    isLoading = false
   }
 
   function displayMovies(movies) {
     const movieList = document.getElementById('movie-list');
-    movieList.innerHTML = ''; // Clear previous results
+    // movieList.innerHTML = ''; // Clear previous results
     movies?.forEach(movie => {
       const movieCard = `
         <div class="movie-card">
@@ -24,8 +32,22 @@ document.getElementById('search-btn').addEventListener('click', () => {
       `;
       movieList.innerHTML += movieCard;
     });
+    if (!movies) {
+      movieList.innerHTML = '<p>No movies found</p>';
+    }
   }
 
-//   if (!movies) {
-//     movieList.innerHTML = '<p>No movies found</p>';
-//   }
+  function handleScroll() {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 500) {
+      console.log("a")
+        if (currentPage < totalPages) {
+            currentPage++;  // Increment to next page
+            const query = document.getElementById('search-input').value;
+            if (query) {
+                searchMovies(query);  // Fetch the next page of results
+                
+              }
+            }
+          }
+        }
+  window.addEventListener('scroll', handleScroll);
